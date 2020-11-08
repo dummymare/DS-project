3b695b70a45214e6a4ce2638e3c8224171c9e134
unknown <shownzhao941@gmail.com>
2020-11-08 14:04:46 +0800

    add svr_rbf

---
+++ regression/SVRrbf.py
@@ -0,0 +1,33 @@
+from azureml.core import Run
+from azureml.core import Dataset
+
+import pandas as pd
+import numpy as np
+
+from sklearn.svm import SVR
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+run = Run.Run.get_context()
+
+dataset = Dataset.get_by_name(ws, name='sales')
+dat = dataset.to_pandas_dataframe()
+
+dat['SALE PRICE'] = np.log(dat['SALE PRICE'])
+dat['LAND SQUARE FEET'] = np.log(dat['LAND SQUARE FEET'])
+dat['GROSS SQUARE FEET'] = np.log(dat['GROSS SQUARE FEET'])
+dat = dat.loc[:, ['ZIP CODE','RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','YEAR BUILT','SALE PRICE','BUILDING CLASS SALE QTF','TAX CODE']]
+
+y = dat['SALE PRICE'].values
+del dat['SALE PRICE']
+x = dat.values
+
+svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
+model = svr_rbf.fit(x, y)
+result = model.predict(x)
+
+run.log("R square", r2_score(y, result))
+run.log("Mean squared error", mean_squared_error(y, result))
+run.log("Mean absolute error", mean_absolute_error(y, result))
+run.complete()
