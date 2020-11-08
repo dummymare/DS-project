e5e49ee2dd8883822e6538ba99f8ec6a2b56066d
unknown <shownzhao941@gmail.com>
2020-11-08 16:11:34 +0800

    add decision tree

---
+++ regression/DTR.py
@@ -0,0 +1,29 @@
+from azureml.core import Run
+import pandas as pd
+import numpy as np
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+run = Run.get_context()
+dat = run.input_datasets['data'].to_pandas_dataframe()
+
+dat['SALE PRICE'] = np.log(dat['SALE PRICE'])
+dat['LAND SQUARE FEET'] = np.log(dat['LAND SQUARE FEET'])
+dat['GROSS SQUARE FEET'] = np.log(dat['GROSS SQUARE FEET'])
+dat = dat.loc[:, ['BOROUGH', 'NEIGHBORHOOD', 'BUILDING CLASS CATEGORY', 'TAX CLASS AT TIME OF SALE', 'RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','YEAR BUILT','SALE PRICE', 'IS BUILDING CLASS CHANGED', 'IS TAX CLASS CHANGED']]
+
+y = dat['SALE PRICE'].values
+del dat['SALE PRICE']
+x = dat.values
+
+DTregressor = regr_2 = DecisionTreeRegressor(min_samples_split=30, min_samples_leaf=10)
+model = DTregressor.fit(x, y)
+result = model.predict(x)
+
+run.log("R square", r2_score(y, result))
+run.log("Mean squared error", mean_squared_error(y, result))
+run.log("Mean absolute error", mean_absolute_error(y, result))
+run.complete()
