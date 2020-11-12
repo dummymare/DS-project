77ee12b5aa1c80174ad88aa16cbf6561980c84b2
unknown <shownzhao941@gmail.com>
2020-11-12 23:28:46 +0800

    amend rbf

--- regression/SVRrbf.py
+++ regression/SVRrbf.py
@@ -9,21 +9,31 @@ from sklearn.metrics import mean_absolute_error
 
 run = Run.get_context()
 dat = run.input_datasets['data'].to_pandas_dataframe()
+testset = run.input_datasets['testset'].to_pandas_dataframe()
 
 dat['SALE PRICE'] = np.log(dat['SALE PRICE'])
 dat['LAND SQUARE FEET'] = np.log(dat['LAND SQUARE FEET'])
 dat['GROSS SQUARE FEET'] = np.log(dat['GROSS SQUARE FEET'])
 dat = dat.loc[:, ['BOROUGH', 'BLOCK', 'LOT', 'ZIP CODE','RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','YEAR BUILT','SALE PRICE','BUILDING CLASS SALE QTF', 'IS BUILDING CLASS CHANGED', 'IS TAX CLASS CHANGED']]
 
+testset['SALE PRICE'] = np.log(testset['SALE PRICE'])
+testset['LAND SQUARE FEET'] = np.log(testset['LAND SQUARE FEET'])
+testset['GROSS SQUARE FEET'] = np.log(testset['GROSS SQUARE FEET'])
+testset = testset.loc[:, ['BOROUGH', 'BLOCK', 'LOT', 'ZIP CODE','RESIDENTIAL UNITS','COMMERCIAL UNITS','TOTAL UNITS','LAND SQUARE FEET','GROSS SQUARE FEET','YEAR BUILT','SALE PRICE','BUILDING CLASS SALE QTF', 'IS BUILDING CLASS CHANGED', 'IS TAX CLASS CHANGED']]
+
 y = dat['SALE PRICE'].values
 del dat['SALE PRICE']
 x = dat.values
 
+y_test = testset['SALE PRICE'].values
+del testset['SALE PRICE']
+x_test = testset.values
+
 svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
 model = svr_rbf.fit(x, y)
-result = model.predict(x)
+result = model.predict(x_test)
 
-run.log("R square", r2_score(y, result))
-run.log("Mean squared error", mean_squared_error(y, result))
-run.log("Mean absolute error", mean_absolute_error(y, result))
+run.log("R square", r2_score(y_test, result))
+run.log("Mean squared error", mean_squared_error(y_test, result))
+run.log("Mean absolute error", mean_absolute_error(y_test, result))
 run.complete()
