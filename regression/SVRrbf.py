fd4afd3f3c81d933c28a3db6a33fa6da3d783493
unknown <shownzhao941@gmail.com>
2020-11-12 22:25:00 +0800

    amend SVRrbf

--- regression/SVRrbf.py
+++ regression/SVRrbf.py
@@ -20,8 +20,9 @@ y = dat['SALE PRICE'].values
 del dat['SALE PRICE']
 x = dat.values
 x = MinMaxScaler(feature_range=(-1,1)).fit(x).transform(x)
+y = MinMaxScaler(feature_range=(-1,1)).fit(y.reshape(-1,1)).transform(y.reshape(-1,1))
 
-svr_rbf = SVR(kernel='rbf', C=10000, gamma=0.1, epsilon=.1)
+svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
 model = svr_rbf.fit(x, y)
 result = model.predict(x)
 
